9a8f4976a59eddbdacebcd528cc32c61fce1a79d
waterandwind <112683966@qq.com>
2019-12-07 20:55:58 +0800

    添加项目

---
+++ tiantong/src/main/java/com/tiantong/tiantong/TiantongApplication.java
@@ -0,0 +1,13 @@
+package com.tiantong.tiantong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiantongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiantongApplication.class, args);
+    }
+
+}
---
+++ tiantong/src/test/java/com/tiantong/tiantong/TiantongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiantong.tiantong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiantongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
