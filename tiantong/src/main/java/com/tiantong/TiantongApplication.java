a9b84a96a3fc9e8fcad773b9d63b8e8a362b924d
00068612 <1126839066@qq.com>
2019-12-26 14:55:42 +0800

    解决跨域跟乱码

--- tiantong/src/main/java/com/tiantong/TiantongApplication.java
+++ tiantong/src/main/java/com/tiantong/TiantongApplication.java
@@ -2,6 +2,7 @@ package com.tiantong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 public class TiantongApplication {
